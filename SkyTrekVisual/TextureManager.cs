ebcab22ee5b03a18c61197c47945923e0f552c91
Pavel Krohmal <Pavel.Krohmal@outlook.com>
2019-05-01 15:01:25 +0300

    Rocket

---
+++ SkyTrekVisual/TextureManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace SkyTrekVisual
+{
+    public static class TextureManager
+    {
+
+        public static BitmapSource[] Rocket_sprites;
+        public static BitmapSource[] Rocket_explosion;
+
+
+        public static void LoadTextures()
+        {
+
+            string[] rocket_sprites = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "Sprites/Rocket/Sprites/", "*.png");
+
+            Rocket_sprites = new BitmapSource[rocket_sprites.Length];
+
+            for (int i = 0; i < rocket_sprites.Length; i++)
+                Rocket_sprites[i] = new BitmapImage(new Uri(rocket_sprites[i], UriKind.Absolute));
+
+
+            string[] rocket_explosion = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "Sprites/Rocket/Explosion/", "*.png");
+
+            Rocket_explosion = new BitmapSource[rocket_explosion.Length];
+
+            for (int i = 0; i < rocket_explosion.Length; i++)
+                Rocket_explosion[i] = new BitmapImage(new Uri(rocket_explosion[i], UriKind.Absolute));
+
+
+        }
+
+
+
+    }
+}
+
