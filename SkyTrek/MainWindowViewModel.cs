6b41863210ca0b464f3e23b6a27e5b41c8602a94
DmitryKovtun <dm_ko@outlook.com>
2019-05-02 23:27:54 +0300

    Всякое разное и вью модель в зачатке

--- SkyTrek/MainWindowViewModel.cs
+++ SkyTrek/MainWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using SkyTrekVisual.Controls.Commands;
 
 namespace SkyTrek
 {
@@ -10,9 +14,120 @@ namespace SkyTrek
 		public MainWindowViewModel()
 		{
 			CurrentDirectory = Directory.GetCurrentDirectory();
+
+
+
+
+
 		}
 
 
+
+		public Engine GameEngine;
+
+
+		public event EventHandler OnGameContinueEvent;
+
+
+
+		private ICommand _ContinueGameCommand;
+
+		public ICommand ContinueGameCommand
+		{
+			get
+			{
+				if(_ContinueGameCommand == null)
+					_ContinueGameCommand = new DelegateCommand(delegate ()
+					{
+						OnGameContinueEvent.Invoke(null,null);
+
+						if(!GameEngine.IsActive())
+							GameEngine.Resume();
+
+					});
+				return _ContinueGameCommand;
+			}
+		}
+
+
+		private ICommand _NewGameCommand;
+
+		public ICommand NewGameCommand
+		{
+			get
+			{
+				if(_NewGameCommand == null)
+					_NewGameCommand = new DelegateCommand(delegate ()
+					{
+
+
+						MessageBox.Show("_NewGameCommand");
+
+
+					});
+				return _NewGameCommand;
+			}
+		}
+
+
+		private ICommand _SettingsCommand;
+
+		public ICommand SettingsCommand
+		{
+			get
+			{
+				if(_SettingsCommand == null)
+					_SettingsCommand = new DelegateCommand(delegate ()
+					{
+
+
+						MessageBox.Show("_SettingsCommand");
+
+
+					});
+				return _SettingsCommand;
+			}
+		}
+
+
+		private ICommand _AboutCommand;
+
+		public ICommand AboutCommand
+		{
+			get
+			{
+				if(_AboutCommand == null)
+					_AboutCommand = new DelegateCommand(delegate ()
+					{
+
+
+						MessageBox.Show("_AboutCommand");
+
+
+					});
+				return _AboutCommand;
+			}
+		}
+
+
+		private ICommand _ExitCommand;
+
+		public ICommand ExitCommand
+		{
+			get
+			{
+				if(_ExitCommand == null)
+					_ExitCommand = new DelegateCommand(delegate ()
+					{
+
+
+						Application.Current.Shutdown();
+
+
+					});
+				return _ExitCommand;
+			}
+		}
 
 
 
