b75135030ac9860c6f99309c673f7faa2aa72f05
Vyacheslav Razumeenko <pro100kenny1@gmail.com>
2019-05-03 18:31:29 +0300

    GameScore class added

---
+++ SkyTrek/GameScore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyTrek
+{
+    class GameScore
+    {
+        public int score { get; set; }
+        public double multiplier { get; set; }
+        public double multiplier_step { get; set; }
+
+        public GameScore()
+        {
+            score = 0;
+            multiplier = 1;
+            multiplier_step = 0.1;
+        }
+
+        public void ScoreChanges(int point)
+        {
+            score += (int)(point * multiplier);
+            MultiplierUpdate();
+        }
+
+        public void MultiplierUpdate()
+        {
+            multiplier += multiplier_step;
+        }
+    }
+}
