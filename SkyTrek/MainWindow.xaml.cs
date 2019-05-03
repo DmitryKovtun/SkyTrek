9d3dc97c736974a095489f5cde82d63d7c99553e
DmitryKovtun <dm_ko@outlook.com>
2019-05-04 01:02:35 +0300

    Биндинг заработанных очков

--- SkyTrek/MainWindow.xaml.cs
+++ SkyTrek/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,24 +21,24 @@ using SkyTrekVisual.GameItems.StarShipList;
 
 namespace SkyTrek
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
 
-    public partial class MainWindow : CustomWindow
-    {
+	public partial class MainWindow : CustomWindow
+	{
 
-        MainWindowViewModel mwvm = new MainWindowViewModel();
-
-	
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            Loaded += MainWindow_Loaded;
+		MainWindowViewModel mwvm = new MainWindowViewModel();
 
 
-            DataContext = mwvm;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Loaded += MainWindow_Loaded;
+
+
+			DataContext = mwvm;
 
 			GameMenu.DataContext = mwvm;
 
@@ -45,21 +46,31 @@ namespace SkyTrek
 
 
 			mwvm.GameEngine.ResetAll();
+
+
 		}
 
 
 
 
 
-        void MainWindow_Loaded(object s, RoutedEventArgs f)
-        {
-            // place for smelly code
-            //MouseDown += delegate (object sender, MouseButtonEventArgs e)
-            //{ Gameplay.GameOver.Visibility = Visibility.Hidden; };
-            //KeyDown += delegate (object sender, KeyEventArgs e)
-            //{ Gameplay.GameOver.Visibility = Visibility.Hidden; };
-            //KeyUp += delegate (object sender, KeyEventArgs e)
-            //{ Gameplay.GameOver.Visibility = Visibility.Hidden; };
+		void MainWindow_Loaded(object s, RoutedEventArgs f)
+		{
+			// place for smelly code
+			MouseDown += delegate (object sender, MouseButtonEventArgs e)
+			{
+				Gameplay.GameOver.Visibility = Visibility.Hidden;
+
+			
+			};
+			KeyDown += delegate (object sender, KeyEventArgs e)
+			{
+				Gameplay.GameOver.Visibility = Visibility.Hidden;
+			};
+			KeyUp += delegate (object sender, KeyEventArgs e)
+			{
+				Gameplay.GameOver.Visibility = Visibility.Hidden;
+			};
 			// end place for smelly code
 
 			mwvm.GameEngine.GameOverEvent += (object sender, EventArgs e) =>
@@ -69,7 +80,7 @@ namespace SkyTrek
 				Gameplay.LabelScore.Visibility = Visibility.Visible;
 				Gameplay.LabelScore.Content = "Score: " + mwvm.GameEngine.speed.Text;
 
-
+			
 			};
 
 			//// now for window
@@ -80,20 +91,23 @@ namespace SkyTrek
 
 
 			// some initialization after we have actual window loaded
-
 			KeyDown += MainWindow_KeyDown;
 
 			GameMenu.IsActive = layoutManager.IsMenu = true;
-
-
-
-
 
 			mwvm.OnGameContinueEvent += Mwvm_OnGameContinueEvent;
 
 			mwvm.CurrentPlayer.OnPlayerHealthChange += CurrentPlayer_OnPlayerHealthChange;
 
+
+			Gameplay.GameBar.DataContext = mwvm.CurrentPlayer.Score;
+
 		}
+
+
+
+
+
 
 		private void CurrentPlayer_OnPlayerHealthChange(object sender, EventArgs e)
 		{
@@ -102,9 +116,11 @@ namespace SkyTrek
 			var f = t.HealthPoints * 252 / 100;
 			Gameplay.GameBar.PlayerHealthIndicator.Width = f > 0 ? f : 0;
 
-			if(f > 23 & f < 100)
+			if(f > 230)
+				Gameplay.GameBar.PlayerHealthIndicator.Background = new BrushConverter().ConvertFromString("#8BC34A") as SolidColorBrush;
+			if(f > 126 & f < 230)
 				Gameplay.GameBar.PlayerHealthIndicator.Background = new BrushConverter().ConvertFromString("#F9AA33") as SolidColorBrush;
-			else
+			else if(f <= 126)
 				Gameplay.GameBar.PlayerHealthIndicator.Background = new BrushConverter().ConvertFromString("#df4e56") as SolidColorBrush;
 
 		}
@@ -117,65 +133,37 @@ namespace SkyTrek
 			layoutManager.IsGameplay = true;
 			GameMenu.IsActive = layoutManager.IsMenu = false;
 
-			
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 		private void MainWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-
-
-
-            if (e.Key == Key.P && layoutManager.IsGameplay)
-            {
+		{
+			if(e.Key == Key.P && layoutManager.IsGameplay)
+			{
 				if(layoutManager.IsPause = !layoutManager.IsPause)
-                     mwvm.GameEngine.Pause();
-                else
+					mwvm.GameEngine.Pause();
+				else
 					mwvm.GameEngine.Resume();
-            }
+			}
 
-            if (e.Key == Key.Escape && layoutManager.IsGameplay && !layoutManager.IsPause)
-            {
-                layoutManager.IsGameplay = false;
+			if(e.Key == Key.Escape && layoutManager.IsGameplay && !layoutManager.IsPause)
+			{
+				layoutManager.IsGameplay = false;
 				GameMenu.IsActive = layoutManager.IsMenu = true;
 
 				mwvm.GameEngine.Pause();
-            }
-
-
-
+			}
 
 		}
 
-       
-			private void Menu_SelectedShipEvent(object sender, EventArgs e)
-			{
-				MessageBox.Show((sender as StarShip).ToString());
-			}
 
+		private void Menu_SelectedShipEvent(object sender, EventArgs e)
+		{
+			MessageBox.Show((sender as StarShip).ToString());
+		}
 
-
-
-    }
+	}
 }
 
---
+++ SkyTrekVisual/GameItems/GameScore.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace SkyTrekVisual.GameItems
+{
+	public class GameScore : INotifyPropertyChanged
+    {
+		public int Score { get; set; } = 10;
+		public double Multiplier { get; set; } = 1;
+		public double MultiplierStep { get; set; } = 0.1;
+
+
+
+//		public double Multiplier { get; set; } = 1;
+
+
+
+
+		public string ScoreString
+		{
+			get { return Score.ToString(); }
+		}
+
+		public string MultiplierString
+		{
+			get { return Math.Round(Multiplier,2).ToString() + "x"; }
+		}
+
+
+
+
+
+
+
+
+		public GameScore()
+		{
+
+		}
+
+		public void Clear()
+		{
+			Score = 0;
+			Multiplier = 0;
+			OnPropertyChanged("ScoreString");
+			OnPropertyChanged("MultiplierString");
+
+		}
+
+
+
+
+		public void ScoreChanges(int point)
+        {
+            Score += (int)(point * Multiplier);
+			OnPropertyChanged("ScoreString");
+
+
+			MultiplierUpdate();
+		}
+
+
+
+
+		public void MultiplierUpdate()
+        {
+            Multiplier += MultiplierStep;
+			OnPropertyChanged("MultiplierString");
+		}
+
+		public void NewShipHit()
+		{
+			MultiplierUpdate();
+
+			ScoreChanges(140);
+		}
+
+		public void NewHit()
+		{
+			ScoreChanges(20);
+		}
+
+
+		public void NewKill()
+		{
+			MultiplierUpdate();
+
+			ScoreChanges(110);
+		}
+
+	
+
+
+
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+
+
+
+
+
+
+
+	}
+}
