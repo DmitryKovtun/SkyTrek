5586879bf75442dc4f70c258a4208dbe2647a642
DmitryKovtun <dm_ko@outlook.com>
2019-05-31 05:20:44 +0300

    Попытка подумать над балансом разных кораблей

---
+++ SkyTrekVisual/Controls/GameBalance/StarShipBalance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyTrekVisual.GameBalance
+{
+
+	//public static List<Balanced
+
+
+	public static class StarShipBalance
+	{
+
+	
+
+
+
+
+
+
+
+
+	}
+}
